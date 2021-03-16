74de20dd6d6fc2fe3c6183ec42c924d0a81d297b
lishijie233 <betatodo@163.com>
2021-03-16 22:38:06 +0800

    second solution

--- src/com/BankMain.java
+++ src/com/BankMain.java
@@ -5,12 +5,19 @@ import com.bank.S001_Two_Sum;
 public class BankMain {
     public static void main(String[] args) {
         S001_Two_Sum ts = new S001_Two_Sum();
-        int[] nums = {1, 3, 5, 7};
+//        int[] nums = {1, 3, 5, 7};// 这里有两种答案
+        int[] nums = {1, 4, 5, 7};
         int target = 8;
-        int[] twoSum = ts.solution1(nums, target);
+        int[] solution1 = ts.solution1(nums, target);
 
-        for(int i=0;i<twoSum.length;i++){
-        System.out.println(twoSum[i]);
+        for(int i=0;i<solution1.length;i++){
+        System.out.println(solution1[i]);
+        }
+        //
+        int[] solution2 = ts.solution2(nums, target);
+
+        for(int i=0;i<solution2.length;i++){
+            System.out.println(solution2[i]);
         }
     }
 }
--- src/com/bank/S001_Two_Sum.java
+++ src/com/bank/S001_Two_Sum.java
@@ -1,4 +1,8 @@
 package com.bank;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Date 2021/3/15 22:05:05
  * @author lishijie
@@ -20,8 +24,15 @@ public class S001_Two_Sum {
         }
         return new int[0];
     }
-    //
-    public void solution2(){
-
+    //降低时间复杂度
+    public int[] solution2(int[] nums, int target){
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hashtable.containsKey(target - nums[i])){
+                return new int[]{hashtable.get(target-nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
